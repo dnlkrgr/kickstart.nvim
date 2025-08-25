3de53c901fe84b2f945553764a96aadba9369f8c
Daniel Krüger <daniel.krueger@libelle.com>
2025-08-25 13:43:28 +0200

    Add moving of tabs and git commands

--- init.lua
+++ init.lua
@@ -163,9 +163,12 @@ vim.opt.scrolloff = 10
 vim.filetype.add {
     extension = {
         lsh = 'lua',
-        cnf = 'dosini'
+        cnf = 'dosini',
+        md = 'markdown'
     },
 }
+
+
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -480,6 +483,11 @@ require('lazy').setup({
             vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
             vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
             vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+            -- git actions
+            vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git status" })
+            vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Git commits" })
+            vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "Git branches" })
 
             -- Slightly advanced example of overriding default behavior and theme
             vim.keymap.set('n', '<leader>/', function()
@@ -879,23 +887,7 @@ require('lazy').setup({
         end,
     },
 
-    { -- You can easily change to a different colorscheme.
-        -- Change the name of the colorscheme plugin below, and then
-        -- change the command in the config to whatever the name of that colorscheme is.
-        --
-        -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-        'folke/tokyonight.nvim',
-        priority = 1000, -- Make sure to load this before all the other start plugins.
-        init = function()
-            -- Load the colorscheme here.
-            -- Like many other themes, this one has different styles, and you could load
-            -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-            vim.cmd.colorscheme 'tokyonight-night'
-
-            -- You can configure highlights by doing something like:
-            vim.cmd.hi 'Comment gui=none'
-        end,
-    },
+    
 
     -- Highlight todo, notes, etc in comments
     { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
@@ -1020,5 +1012,14 @@ vim.opt.expandtab = true -- Use spaces instead of tabs when editing
 vim.opt.shiftwidth = 4   -- Number of spaces to use for each step of (auto)indent
 vim.opt.tabstop = 4      -- Number of spaces that a <Tab> in the file counts for
 
+
+vim.opt.termguicolors = true
+
 -- vim.treesitter.language.register('markdown', 'markdown')
 -- vim.treesitter.language.register('markdown_inline', 'markdown')
+
+
+-- Move tab right
+vim.keymap.set('n', '<leader>tr', ':tabm +1<CR>', { desc = 'Move tab right' })
+-- Move tab left
+vim.keymap.set('n', '<leader>tl', ':tabm -1<CR>', { desc = 'Move tab left' })
